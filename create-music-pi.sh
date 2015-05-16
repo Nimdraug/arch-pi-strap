5cabdf2cd4560c1258cb5ed02eb4debb47e92748
Martin Hulten-Ashauer <martin@mortos.org>
2015-05-16 17:42:10 +1000

    auto create archlinux-arm installation on sda drive

---
+++ create-music-pi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Create partitions
+DEV=/dev/sda
+
+parted -s $DEV mklabel msdos
+parted -s $DEV mkpart primary fat16 1049kB 106
+parted -s $DEV mkpart primary ext4 106 100%
+
+mkfs.ext4 -F ${DEV}2
+
+parted $DEV print
+
+mkdir -p root
+
+mount ${DEV}2 m
+
+mkdir -p root/boot
+
+mount ${DEV}1 root/boot
+
+wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
+
+bsdtar -xpf ArchLinuxARM-rpi-latest.tar.gz -C root
+
+umount root/boot
+umount root
+
+rm -rf root
