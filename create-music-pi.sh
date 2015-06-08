d6f2788190648b4a850e10d3006064b1555495b6
Martin Hulten-Ashauer <martin@mortos.org>
2015-06-08 17:03:38 +1000

    each step turned into a function

--- create-music-pi.sh
+++ create-music-pi.sh
@@ -1,42 +1,71 @@
 #!/bin/bash
 
+function create_part_table
+{
+    parted -s $1 mklabel msdos
+    parted -s $1 mkpart primary fat16 1049kB 106
+    parted -s $1 mkpart primary ext4 106 100%
+}
+
+function make_filesystems
+{
+    mkfs.fat ${1}1
+    mkfs.ext4 -F ${1}2
+}
+
+function setup_target
+{
+    mkdir -p root
+
+    mount ${1}2 root
+
+    mkdir -p root/boot
+
+    mount ${1}1 root/boot
+}
+
+function download_latest
+{
+    wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
+}
+
+function install_base
+{
+    bsdtar -xvpf ArchLinuxARM-rpi-latest.tar.gz -C root
+}
+
+function install_packages
+{
+    arch-install-scripts/pacstrap -c root/ $*
+}
+
+function install_extra
+{
+    mkdir -p root/media
+}
+
+function cleanup_target
+{
+    # Unmount the SD
+    umount -R root
+
+    # Cleanup the mountpoint
+    rm -rf root
+}
+
+function all
+{
+    create_part_table $1
+    make_filesystems $1
+    setup_target $1
+    download_latest
+    install_base
+    install_packages mpd mpc
+    install_extra
+    cleanup_target
+}
+
 # !!! Change to the correct sd-card device !!!
 DEV=/dev/sda
 
-# Create partitions
-parted -s $DEV mklabel msdos
-parted -s $DEV mkpart primary fat16 1049kB 106
-parted -s $DEV mkpart primary ext4 106 100%
-
-# Make filesystems
-mkfs.fat ${DEV}1
-mkfs.ext4 -F ${DEV}2
-
-parted $DEV print
-
-# Mount the SD
-mkdir -p root
-
-mount ${DEV}2 root
-
-mkdir -p root/boot
-
-mount ${DEV}1 root/boot
-
-# Download the latest ArchLinuxARM release
-wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
-
-# Extract it to the mounted SD
-bsdtar -xvpf ArchLinuxARM-rpi-latest.tar.gz -C root
-
-# Install required packages
-arch-install-scripts/pacstrap -c root/ mpd mpc
-
-# Create other required dirs
-mkdir -p root/media
-
-# Unmount the SD
-umount -R root
-
-# Cleanup the mountpoint
-rm -rf root
+all $DEV
